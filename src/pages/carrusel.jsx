640d4ccdf890e09742dd1ee53643ee60a64f00ce
pedro imbrech <elprogramadorpedro@gmail.com>
2024-03-20 14:03:54 -0300

    renderizacao do lado do servidorr

---
+++ src/pages/carrusel.jsx
@@ -0,0 +1,31 @@
+// pages/carrusel.tsx
+
+import { NextPage } from "next";
+import Head from "next/head";
+
+const Carrusel: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Carrinho</title>
+        <meta name="description" content="Meu carrinho de compras" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>
+        Carrinho
+        oiiiiiiiiiiiiiiiiiiiiiiiiiii
+      </h1>
+    </>
+  )
+}
+
+export default Carrusel
+
+
+
+
+
+
+
+
